1cec06f818f474375b197b13510e4f32b2000f66
Harold Affo <affoharold@yahoo.fr>
2016-11-14 09:01:16 -0500

    added logging

--- src/main/java/gov/nist/mml/oar/ds/config/Application.java
+++ src/main/java/gov/nist/mml/oar/ds/config/Application.java
@@ -1,16 +1,24 @@
 package gov.nist.mml.oar.ds.config;
  
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @RefreshScope
 @ComponentScan(basePackages = {"gov.nist.mml.oar.ds"} )
+@EnableEurekaClient
 public class Application {
 
+	private static Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String... args) {
+    	log.info("########## Starting oar distribution service ########");
 		SpringApplication.run(Application.class, args);
 	}
 }
