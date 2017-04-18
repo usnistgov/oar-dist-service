e22f12c674370976e4f46c2b87661e1461e54abb
Harold Affo <harold.affo@nist.gov>
2017-04-18 16:38:32 -0400

    Update aftercicd.sh

--- scripts/aftercicd.sh
+++ scripts/aftercicd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/oar-docker/dist-service
-if [[ $(sudo docker ps -a | grep "dist-service") ]]; then
-    sudo docker rm -f $(sudo docker ps -a | grep "dist-service")
+if [[ $(sudo docker ps -aqf "name=dist-service") ]]; then
+    sudo docker rm -f $(sudo docker ps -aqf "name=dist-service")
 fi
-if [[ $(sudo docker images -a | grep "dist-service") ]]; then
-   sudo docker rmi -f $(sudo docker images -a | grep "dist-service")
+if [[ $(sudo docker images dist-service -aq) ]]; then
+   sudo docker rmi -f $(sudo docker images dist-service -aq)
 fi
 sudo docker-compose up -d --build
