f8c63013bc30264cc6c941940892131958a33eeb
Harold Affo <harold.affo@nist.gov>
2017-05-25 11:16:52 -0400

    Update DownloadController.java

--- src/main/java/gov/nist/oar/ds/controller/DownloadController.java
+++ src/main/java/gov/nist/oar/ds/controller/DownloadController.java
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
 import gov.nist.oar.ds.service.DownloadService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
