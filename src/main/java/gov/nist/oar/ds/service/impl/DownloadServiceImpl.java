7090832b7b7de0336e391077b2394ce4950913c0
skolli853780 <som.kolli@gmail.com>
2017-06-02 11:56:10 -0400

    Update the Zip service to follow redirect

--- src/main/java/gov/nist/oar/ds/service/impl/DownloadServiceImpl.java
+++ src/main/java/gov/nist/oar/ds/service/impl/DownloadServiceImpl.java
@@ -15,40 +15,24 @@ package gov.nist.oar.ds.service.impl;
 
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -63,23 +47,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-
-
-import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import gov.nist.oar.ds.s3.S3Wrapper;
 import gov.nist.oar.ds.service.DownloadService;
@@ -193,7 +166,6 @@ public class DownloadServiceImpl implements DownloadService {
   public ResponseEntity<byte[]>  downloadZipFile(String id) throws Exception {
     
     try {    
-      HttpServletRequest request = null;
       
       CloseableHttpClient httpClient = HttpClientBuilder
           .create()
@@ -201,7 +173,6 @@ public class DownloadServiceImpl implements DownloadService {
       HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
       factory.setBufferRequestBody(false);
       RestTemplate restTemplate = new RestTemplate(factory);
-
       HttpHeaders headers = new HttpHeaders();
       headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
       HttpEntity<String> entity = new HttpEntity<String>(headers);
@@ -215,18 +186,12 @@ public class DownloadServiceImpl implements DownloadService {
       JSONObject jsonRecord = response.getBody();
       String jsonResultData = new Gson().toJson(jsonRecord.get("ResultData"));
       JSONParser parser = new JSONParser(); 
-      
       JSONArray jsonArrayResultData = (JSONArray) parser.parse(jsonResultData);
-
       JSONObject object = (JSONObject) jsonArrayResultData.get(0);
-
       String jsonComponents= new Gson().toJson(object.get("components"));
-
       JSONArray jsonArrayComponents = (JSONArray) parser.parse(jsonComponents);
-      
       byte[] myBytes = null;
       myBytes = getCompressed(jsonArrayComponents,fileName);
-           
       HttpHeaders httpHeaders = new HttpHeaders();
       httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
       httpHeaders.setContentLength(myBytes.length);
@@ -245,47 +210,35 @@ public class DownloadServiceImpl implements DownloadService {
     
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
     ZipOutputStream zos = new ZipOutputStream(bos);
-    InputStream in = null;
-    BufferedInputStream entryStream = null;
-
-    CloseableHttpClient httpClient = HttpClientBuilder
-        .create()
-        .build();
-    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-    factory.setBufferRequestBody(false);
-    RestTemplate restTemplate = new RestTemplate(factory);
-    restTemplate.getMessageConverters().add(
-            new ByteArrayHttpMessageConverter());
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-    HttpEntity<String> entity = new HttpEntity<String>(headers);
+    CloseableHttpClient httpclient = HttpClients.createDefault();
     for (int i = 0; i < json.size(); i++) {
       JSONObject jsonObjComp = (JSONObject) json.get(i);
       if (jsonObjComp.containsKey("@type"))
         if(jsonObjComp.get("@type").toString().contains("nrdp:DataFile"))
           {
+            logger.info("after json object--" + jsonObjComp.get("downloadURL"));
+            logger.info("after json object--" + jsonObjComp.get("title"));
             if (jsonObjComp.containsKey("downloadURL"))
             {
-              logger.info("Title -" + jsonObjComp.get("title").toString());
-              logger.info("DownloadURL - " + jsonObjComp.get("downloadURL").toString());
-              ResponseEntity<byte[]> response = restTemplate.exchange(
-                  jsonObjComp.get("downloadURL").toString(),
-                      HttpMethod.GET, entity, byte[].class, "1");
-              
-              ZipEntry entry = new ZipEntry(fileName + "/" + jsonObjComp.get("filepath").toString());
-              if (response.getStatusCode() == HttpStatus.OK) 
-                {
+              HttpGet request = new HttpGet(jsonObjComp.get("downloadURL").toString());
+              CloseableHttpResponse response = httpclient.execute(request);
+              try {
+                BufferedInputStream bis = new BufferedInputStream(response.getEntity().getContent());
+                  ZipEntry entry = new ZipEntry(fileName + "/" + jsonObjComp.get("filepath").toString());
                   zos.putNextEntry( entry );
-                  zos.write(response.getBody());
+                  int inByte;
+                  while((inByte = bis.read()) != -1) {
+                    zos.write(inByte);
+                  }
+                  bis.close();
+                } finally {
+                  response.close();
                   zos.closeEntry();
                 }
             }
           }
       }
       zos.close();
-      
       return bos.toByteArray();
   }
-  
 }
