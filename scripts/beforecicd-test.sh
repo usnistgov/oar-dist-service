b1e02033edf96099eb2e7302e57324bf55314222
Harold Affo <harold.affo@nist.gov>
2017-06-20 15:22:08 -0400

    Delete beforecicd-test.sh

--- scripts/beforecicd-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo rm -r /opt/data/backup/oar-dist-service/
-if [ -f /home/ubuntu/oar-docker/dist-service/oar-dist-service.jar ];
-then
-  #backup previous build
-  sudo cp -r /home/ubuntu/oar-docker/dist-service/oar-dist-service.jar /opt/data/backup/oar-dist-service/
-  #remove previous build
-  sudo rm -r /home/ubuntu/oar-docker/dist-service/oar-dist-service.jar
-fi
