0a58640c62d3ec26e57c0f1a9fd22336fcb713df
Harold Affo <harold.affo@nist.gov>
2017-06-27 13:26:30 -0400

    Update aftercicd.sh

--- scripts/test/aftercicd.sh
+++ scripts/test/aftercicd.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/oar-docker/apps/
-if [[ $(sudo docker ps -aqf "name=dist-service") ]]; then
-    sudo docker rm -f $(sudo docker ps -aqf "name=dist-service")
-fi
-if [[ $(sudo docker images dist-service -aq) ]]; then
-   sudo docker rmi -f $(sudo docker images dist-service -aq)
-fi
-
-sudo ./deploy-test.sh
+sudo /home/ubuntu/oar-docker/apps/dist-service/deploy-test.sh
