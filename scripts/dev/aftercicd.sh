9521944054d57744fcd4393ef57a2389dbc5d1ba
Harold Affo <harold.affo@nist.gov>
2017-06-27 13:26:56 -0400

    Update aftercicd.sh

--- scripts/dev/aftercicd.sh
+++ scripts/dev/aftercicd.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sudo /home/ubuntu/oar-docker/apps/dist-service/clear.sh
-sudo /home/ubuntu/oar-docker/apps/deployAll-dev.sh
-
+sudo /home/ubuntu/oar-docker/apps/dist-service/deploy-dev.sh
