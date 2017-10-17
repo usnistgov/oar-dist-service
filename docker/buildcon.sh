5b84533f5b064637d6ad08c718e18d86db68ae07
Ray Plante <raymond.plante@nist.gov>
2017-10-17 11:55:41 -0400

    docker: build software as user running docker

--- docker/buildcon.sh
+++ docker/buildcon.sh
@@ -9,8 +9,12 @@ codedir=`(cd $execdir/.. > /dev/null 2>&1; pwd)`
 set -e
 
 PACKAGE_NAME=oar-dist-service
+CON_USER=`id --user --name`
+CON_UID=`id --user`
 
 cd $execdir
 set -x
-docker build -t $PACKAGE_NAME/build-test build-test 2>&1 | tee -a buildcon.log
+docker build -t $PACKAGE_NAME/build-test build-test \
+             --build-arg=user=$CON_USER --build-arg=uid=$CON_UID 2>&1 \
+    | tee -a buildcon.log
 
