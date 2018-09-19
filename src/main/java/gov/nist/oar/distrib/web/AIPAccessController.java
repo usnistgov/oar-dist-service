1630f87ce267eb85ae3e1a37ff2733bc02442cd9
Ray Plante <raymond.plante@nist.gov>
2018-09-19 10:16:55 -0400

    added NISTDistribServiceConfig, AIPAccessController (with tests)

---
+++ src/main/java/gov/nist/oar/distrib/web/AIPAccessController.java
@@ -0,0 +1,177 @@
+/**
+ * This software was developed at the National Institute of Standards and Technology by employees of
+ * the Federal Government in the course of their official duties. Pursuant to title 17 Section 105
+ * of the United States Code this software is not subject to copyright protection and is in the
+ * public domain. This is an experimental system. NIST assumes no responsibility whatsoever for its
+ * use by other parties, and makes no guarantees, expressed or implied, about its quality,
+ * reliability, or any other characteristic. We would appreciate acknowledgement if the software is
+ * used. This software can be redistributed and/or modified freely provided that any derivative
+ * works bear some notice that they are derived from it, and any modified versions bear some notice
+ * that they have been modified.
+ */
+package gov.nist.oar.distrib.web;
+
+import gov.nist.oar.distrib.service.PreservationBagService;
+import gov.nist.oar.distrib.FileDescription;
+import gov.nist.oar.distrib.StreamHandle;
+import gov.nist.oar.distrib.ResourceNotFoundException;
+import gov.nist.oar.distrib.DistributionException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Api;
+import springfox.documentation.annotations.ApiIgnore;
+
+/**
+ * A web service controller that provides access to Archive Information Package (AIP) files.
+ * <p>
+ * In the OAR PDR model, an <em>archive information package</em> (AIP) is made up of one or more files.  
+ * Together, these make up the AIP.  AIP files have unique names within the service; if one knows 
+ * the name, one can access it.  In this service implementation, AIP files are in the form of 
+ * serialized <em>preservation bags</em>--files representing BagIt-formatted data packages.  
+ * <p>
+ * This controller allows access to an AIP file based on its name, allowing download of the 
+ * file itself or metadata about it (encoded in JSON).  It provides a web front end to bag file 
+ * access provided by the 
+ * {@link gov.nist.oar.distrib.service.PreservationBagService PreservationBagService}.  (Note 
+ * that discovery of bag file names and head bags, courtesy 
+ * of the {@link gov.nist.oar.distrib.service.PreservationBagService PreservationBagService}, is 
+ * provided via the DatasetAccessController.)
+ *
+ * @author Raymond Plante
+ */
+@RestController
+@Api
+@RequestMapping(value = "/ds/_aip")
+public class AIPAccessController {
+
+    Logger logger = LoggerFactory.getLogger(AIPAccessController.class);
+
+    @Autowired
+    PreservationBagService pres;
+
+    @Value("${distrib.baseurl}")
+    String svcbaseurl;
+
+    /**
+     * stream out the AIP file to the web client.
+     * @param name   the name of the desired AIP file
+     * @param resp   the response object.  This will be used to set the response header and 
+     *               stream the desired file.
+     */
+    @ApiOperation(value = "Return an AIP file (e.g. a preservation bag)", nickname = "getAIPFile",
+                  notes = "return the AIP with the given name")
+    @RequestMapping(value = "/{name}")
+    public void downloadAIP(@PathVariable("name") String name,
+                            @ApiIgnore HttpServletResponse resp)
+        throws ResourceNotFoundException, DistributionException, IOException
+    {
+        StreamHandle sh = null;
+        try {
+            sh = pres.getBag(name);
+
+            if (sh.getInfo().contentType == null || sh.getInfo().contentType.equals(""))
+                sh.getInfo().contentType = MediaType.APPLICATION_OCTET_STREAM.toString();
+
+            logger.info("AIP requested: " + sh.getInfo().name);
+
+            /*
+             * Need encodeDigest implementat that converts hex to base64
+             *
+            if (sh.getInfo().checksum != null) 
+                response.setHeader("Digest", encodeDigest(sh.getInfo().checksum));
+             */
+            resp.setHeader("Content-Length", Long.toString( sh.getInfo().contentLength ));
+            resp.setHeader("Content-Type", sh.getInfo().contentType);
+            resp.setHeader("Content-Disposition", "filename=\"" +
+                           Pattern.compile("/+").matcher(name).replaceAll("_") + "\"");
+
+            int len;
+            byte[] outdata = new byte[100000];
+            OutputStream out = resp.getOutputStream();
+
+            try {
+                while ((len = sh.dataStream.read(outdata)) != -1) {
+                    out.write(outdata, 0, len);
+                }
+                resp.flushBuffer();
+                logger.info("AIP delivered: " + sh.getInfo().name);
+            }
+            catch (org.apache.catalina.connector.ClientAbortException ex) {
+                logger.info("Client cancelled the download");
+                // resp.flushBuffer();
+            }
+            catch (IOException ex) {
+                logger.debug("IOException type: "+ex.getClass().getName());
+
+                // "Connection reset by peer" gets thrown if the user cancels the download
+                if (ex.getMessage().contains("Connection reset by peer")) {
+                    logger.info("Client cancelled download");
+                } else {
+                    logger.error("IO error while sending file, " + name +
+                                 ": " + ex.getMessage());
+                    throw ex;
+                }
+            }
+        }
+        catch (FileNotFoundException ex) {
+            // send 404
+            throw new ResourceNotFoundException("Failed to find AIP file, "+name);
+        }
+        finally {
+            if (sh != null)
+                sh.close();
+        }
+    }
+
+    /**
+     * return a description of the AIP with the given name
+     * @param name   the name of the desired AIP file
+     */
+    @ApiOperation(value = "describe an AIP file (e.g. a preservation bag)", nickname = "describeAIPFile",
+                  notes = "return a description of the AIP with the given name as a JSON object")
+    @RequestMapping(value = "/{name}/_info")
+    public FileDescription describeAIP(@PathVariable("name") String name)
+        throws ResourceNotFoundException, DistributionException
+    {
+        try {
+            FileDescription out = pres.getInfo(name);
+            if (svcbaseurl != null && ! svcbaseurl.equals("") && out.name != null) {
+                try {
+                    // set a download URL for the file being described as a property
+                    URL dlurl = new URL(svcbaseurl);
+                    dlurl = new URL(dlurl, "_aip/"+name);
+                    out.setProp("downloadURL", dlurl.toString());
+                } catch (MalformedURLException ex) {
+                    // can happen if svcbaseurl is malformed
+                    logger.error("Not interpretable as a base URL: "+svcbaseurl);
+                }
+            }
+            return out;
+        }
+        catch (FileNotFoundException ex) {
+            // return 404
+            throw new ResourceNotFoundException("AIP file not found: "+name);
+        }
+    }
+
+}
---
+++ src/main/java/gov/nist/oar/distrib/web/NISTDistribServiceConfig.java
@@ -0,0 +1,189 @@
+/**
+ * This software was developed at the National Institute of Standards and Technology by employees of
+ * the Federal Government in the course of their official duties. Pursuant to title 17 Section 105
+ * of the United States Code this software is not subject to copyright protection and is in the
+ * public domain. This is an experimental system. NIST assumes no responsibility whatsoever for its
+ * use by other parties, and makes no guarantees, expressed or implied, about its quality,
+ * reliability, or any other characteristic. We would appreciate acknowledgement if the software is
+ * used. This software can be redistributed and/or modified freely provided that any derivative
+ * works bear some notice that they are derived from it, and any modified versions bear some notice
+ * that they have been modified.
+ */
+package gov.nist.oar.distrib.web;
+
+import gov.nist.oar.distrib.LongTermStorage;
+import gov.nist.oar.distrib.storage.AWSS3LongTermStorage;
+import gov.nist.oar.distrib.storage.FilesystemLongTermStorage;
+import gov.nist.oar.distrib.service.FileDownloadService;
+import gov.nist.oar.distrib.service.FromBagFileDownloadService;
+import gov.nist.oar.distrib.service.PreservationBagService;
+import gov.nist.oar.distrib.service.DefaultPreservationBagService;
+
+import java.io.InputStream;
+import java.io.FileNotFoundException;
+import javax.activation.MimetypesFileTypeMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.regions.RegionUtils;
+    
+
+/**
+ * The configuration for the Distribution Service used in the NIST Public Data Repository.
+ * <p>
+ * The class defines how the components are put together to form the distribution web service
+ * using the Spring Boot framework.  The following configuration properties are supported for 
+ * driving the configuration:
+ * <dl>
+ *   <dt> {@code distrib.bagstore.mode} </dt>
+ *   <dd> a label that indicates how preservation bags are stored; the following values are 
+ *        supported:
+ *        <dl>
+ *          <dd> <strong>local</strong> -- the bags are stored under a local directory (specified 
+ *               by {@code distrib.bagstore.location}); the {@code FilesystemLongTermStorage} 
+ *               class will be used to access the bags.  </dd>
+ *          <dd> <strong>aws</strong> -- the bags are stored in an AWS S3 bucket (whose name
+ *               is specified by {@code distrib.bagstore.location}), assuming the distribution
+ *               service itself is running on an AWS EC2 machine.  The {@code AWSS3LongTermStorage} 
+ *               class will be used to access the bags, and implicit credentials through the 
+ *               EC2 machine will be used.  </dd>
+ *          <dd> <strong>remote</strong> -- the bags are stored in an AWS S3 bucket (whose name
+ *               is specified by {@code distrib.bagstore.location}), assuming the distribution
+ *               service itself is running on an AWS EC2 machine.  The {@code AWSS3LongTermStorage} 
+ *               class will be used to access the bags; however, credentials must be explicitly 
+ *               provided. <em>Not yet implemented; will throw exception.</em> </dd>
+ *        </dl> 
+ *        All other values will throw an exception.  </dd>
+ *   <dt> {@code distrib.bagstore.location} </dt>
+ *   <dd> The location where the preservation bags are stored.  If {@code distrib.bagstore.mode}
+ *        is set to {@code local}, this value is the path to a directory on the local filesystem.
+ *        If {@code distrib.bagstore.mode} is set to {@code aws} or {@code remote}, this value is 
+ *        the name of an S3 bucket. </dd>
+ * </dl>
+ * <p>
+ * See also 
+ * {@see gov.nist.oar.distrib.web.LocalstackDistribServiceConfig LocalstackDistribServiceConfig}
+ */
+@SpringBootApplication
+public class NISTDistribServiceConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(NISTDistribServiceConfig.class);
+
+    /**
+     * the S3 bucket where preservation bags are located
+     */
+    @Value("${distrib.bagstore.location}")
+    String bagstore;
+
+    /**
+     * the storage mode of which three are recognized:
+     * <ul>
+     *   <li> {@code aws} - Bags are stored in an S3 bucket and accessed from an EC2 machine 
+     *                      (production mode); in this case, bagstore holds the S3 bucket name. </li>
+     *   <li> {@code remote} - Bags are stored in an S3 bucket and accessed from a remote (non-EC2) 
+     *                       machine (a development mode); in this case, bagstore holds the S3 bucket 
+     *                       name. </li>
+     *   <li> {@code local}  - Bags are stored under a directory on the local filesystem
+     *                       (testing or development mode); in this case, bagstore identifies the 
+     *                       bags' parent directory.  </li>
+     */
+    @Value("${distrib.bagstore.mode}")
+    String mode;
+
+    /**
+     * the AWS region the service should operate in; this is ignored if mode=local.
+     */
+    @Value("{cloud.aws.region}")
+    String region;
+
+    @Autowired LongTermStorage          lts;    // set via getter below
+    @Autowired MimetypesFileTypeMap mimemap;    // set via getter below
+
+    /**
+     * the storage service to use to access the bags
+     */
+    @Bean
+    public LongTermStorage getLongTermStorage() throws ConfigurationException {
+        try {
+            if (mode.equals("aws") || mode.equals("remote")) 
+                return new AWSS3LongTermStorage(bagstore, getAmazonS3());
+            else if (mode.equals("local")) 
+                return new FilesystemLongTermStorage(bagstore);
+            else
+                throw new ConfigurationException("distrib.bagstore.mode",
+                                                 "Unsupported storage mode: "+ mode);
+        }
+        catch (FileNotFoundException ex) {
+            throw new ConfigurationException("distrib.bagstore.location",
+                                             "Storage Location not found: "+ex.getMessage(), ex);
+        }
+    }
+
+    /**
+     * the client for access S3 storage
+     */
+    public AmazonS3 getAmazonS3() throws ConfigurationException {
+        logger.info("Creating S3 client");
+
+        if (mode.equals("remote"))
+            throw new ConfigurationException("Remote credentials not supported yet");
+
+        // import credentials from the EC2 machine we are running on
+        InstanceProfileCredentialsProvider provider = InstanceProfileCredentialsProvider.getInstance();
+
+        AmazonS3 client = AmazonS3Client.builder()
+                                        .standard()                 
+                                        .withCredentials(provider)
+                                        .build();
+        client.setRegion(RegionUtils.getRegion(region));
+        return client;
+    }
+
+    /**
+     * the MIME type assignments to use when setting content types
+     */
+    @Bean
+    public MimetypesFileTypeMap getMimetypesFileTypeMap() {
+        InputStream mis = getClass().getResourceAsStream("/mime.types");
+        if (mis == null) {
+            logger.warn("No mime.type resource found; content type support will be limited!");
+            return new MimetypesFileTypeMap();
+        }
+        return new MimetypesFileTypeMap(mis);
+    }
+
+    /**
+     * the service implementation to use to download data products.
+     */
+    @Bean
+    public FileDownloadService getFileDownloadService() {
+        return new FromBagFileDownloadService(lts, mimemap);
+    }
+        
+    /**
+     * the service implementation to use to download data products.
+     */
+    @Bean
+    public PreservationBagService getPreservationBagService() {
+        return new DefaultPreservationBagService(lts, mimemap);
+    }
+
+    /**
+     * the spring-boot application main()
+     */
+    public static void main(String[] args) {
+        // run with the configuration set here
+        SpringApplication.run( NISTDistribServiceConfig.class, args );
+    }
+}
+
