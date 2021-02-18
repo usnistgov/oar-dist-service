74e72a2cae3468deb3a6a655eee98a13b7046f43
deoyani <deoyani.pics@gmail.com>
2021-02-18 13:41:28 -0500

    Updated distribution url with the requestid in bundle plan.

--- src/main/java/gov/nist/oar/distrib/web/DatasetAccessController.java
+++ src/main/java/gov/nist/oar/distrib/web/DatasetAccessController.java
@@ -33,6 +33,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.HandlerMapping;
@@ -335,7 +336,8 @@ public class DatasetAccessController {
     @GetMapping(value = "/ark:/{naan:\\d+}/{dsid}/**")
     public void downloadFileViaARK(@PathVariable("dsid") String dsid, @PathVariable("naan") String naan,
                                    @ApiIgnore HttpServletRequest request,
-                                   @ApiIgnore HttpServletResponse response)
+                                   @ApiIgnore HttpServletResponse response,
+                                   @RequestParam String requestId)
         throws ResourceNotFoundException, FileNotFoundException, DistributionException, IOException
     {
         logger.debug("Matched ARK ID for download: ark:/"+naan+"/"+dsid);
@@ -423,7 +425,7 @@ public class DatasetAccessController {
     @ApiOperation(value = "stream the data product with the given name", nickname = "get file", notes = "This is the primary way to download a data file")
     @GetMapping(value = "/{dsid:[^a][^r][^k][^:].*}/**")
     public void downloadFile(@PathVariable("dsid") String dsid, @ApiIgnore HttpServletRequest request,
-                             @ApiIgnore HttpServletResponse response)
+                             @ApiIgnore HttpServletResponse response, @RequestParam String requestId)
         throws ResourceNotFoundException, FileNotFoundException, DistributionException, IOException
     {
 	
@@ -473,12 +475,11 @@ public class DatasetAccessController {
 	checkDatasetID(dsid);
 	checkFilePath(filepath);
 
-	if (logger.isInfoEnabled()) {
+
 	    String msg = "Data File requested: " + dsid + "/" + filepath;
 	    if (version != null)
 		msg += " (version " + version + ")";
 	    logger.info(msg);
-        }
 	
 	StreamHandle sh = null;
 	try {
