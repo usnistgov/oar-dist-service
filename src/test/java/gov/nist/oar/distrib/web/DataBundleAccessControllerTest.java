d5f8d80a5beb80d0450908e387a61494c5302b91
Deoyani Heinis <deoyani.pics@gmail.com>
2022-05-10 11:24:44 -0400

    Updated the test to make sure changes in bundle controller and taken care of.

--- src/test/java/gov/nist/oar/distrib/web/DataBundleAccessControllerTest.java
+++ src/test/java/gov/nist/oar/distrib/web/DataBundleAccessControllerTest.java
@@ -90,8 +90,8 @@ public class DataBundleAccessControllerTest {
 	inputfileList[0] = testval1;
 	inputfileList[1] = testval2;
 	BundleRequest bFL = new BundleRequest("testdownload-1", inputfileList,0,2);
-	RequestEntity<BundleRequest> request = RequestEntity.post(new URI(getBaseURL() + "/ds/_bundle")).body(bFL);
-
+	RequestEntity<BundleRequest> request = RequestEntity.post(new URI(getBaseURL() + "/ds/_bundle")).header("X-Real-IP", "test").body(bFL);
+   
 	ResponseEntity<String> response = websvc.exchange(request, String.class);
 	System.out.println("response.getStatusCode():" + response.getStatusCode() + " \n resp.getHeaders():"
 		+ response.getHeaders() + "\n response.getBody().length():" + response.getBody().length());
