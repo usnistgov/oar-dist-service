51dc20656900003e0b783f8c38bb15dc7dcb12e2
elmiomar <omarilias.elmimouni@nist.gov>
2023-01-12 03:34:31 -0500

    add bean for RPAConfiguration

--- src/main/java/gov/nist/oar/distrib/web/RPAConfiguration.java
+++ src/main/java/gov/nist/oar/distrib/web/RPAConfiguration.java
@@ -6,6 +6,7 @@ import lombok.Value;
 import lombok.experimental.NonFinal;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.Map;
 @Value
 @NonFinal
 @Validated
+@Component
 @ConfigurationProperties(prefix = "rpa")
 public class RPAConfiguration {
 
