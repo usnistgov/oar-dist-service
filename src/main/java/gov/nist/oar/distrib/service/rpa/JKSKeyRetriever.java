0336daf5f7152ad724f3e8cca6140f730cd4db6e
elmiomar <omarilias.elmimouni@nist.gov>
2023-01-24 03:53:55 -0500

    add private key retriever implementations

--- src/main/java/gov/nist/oar/distrib/service/rpa/JKSKeyRetriever.java
+++ src/main/java/gov/nist/oar/distrib/service/rpa/JKSKeyRetriever.java
@@ -1,2 +1,25 @@
-package gov.nist.oar.distrib.service.rpa;public class JKSKeyRetriever {
+package gov.nist.oar.distrib.service.rpa;
+
+import gov.nist.oar.distrib.web.RPAConfiguration;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.Key;
+import java.security.KeyStore;
+
+public class JKSKeyRetriever implements KeyRetriever {
+    @Override
+    public Key getKey(RPAConfiguration rpaConfiguration)  {
+        try {
+            KeyStore keystore = KeyStore.getInstance(rpaConfiguration.getJksConfig().getKeyStoreType());
+            keystore.load(Files.newInputStream(
+                            Paths.get(rpaConfiguration.getJksConfig().getKeyStorePath())),
+                    rpaConfiguration.getJksConfig().getKeyStorePassword().toCharArray());
+            return keystore.getKey(
+                    rpaConfiguration.getJksConfig().getKeyAlias(),
+                    rpaConfiguration.getJksConfig().getKeyPassword().toCharArray()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Exception during key generation");
+        }
+    }
 }
--- src/main/java/gov/nist/oar/distrib/service/rpa/KeyRetriever.java
+++ src/main/java/gov/nist/oar/distrib/service/rpa/KeyRetriever.java
@@ -1,2 +1,19 @@
-package gov.nist.oar.distrib.service.rpa;public interface KeyRetriever {
+package gov.nist.oar.distrib.service.rpa;
+
+import gov.nist.oar.distrib.web.RPAConfiguration;
+
+import java.security.Key;
+
+
+/**
+ * Interface to retrieve the private key.
+ */
+public interface KeyRetriever {
+
+    /**
+     * Get a private key.
+     *
+     * @param rpaConfiguration - the RPA configuration
+     */
+    Key getKey(RPAConfiguration rpaConfiguration);
 }
