2cf530eaeeaa70812e13843c0ab77cb0d2c77bcd
elmiomar <omarilias.elmimouni@nist.gov>
2023-01-25 01:21:15 -0500

    log expection message in keyretriever and keystore path

--- src/main/java/gov/nist/oar/distrib/service/rpa/JKSKeyRetriever.java
+++ src/main/java/gov/nist/oar/distrib/service/rpa/JKSKeyRetriever.java
@@ -1,15 +1,21 @@
 package gov.nist.oar.distrib.service.rpa;
 
 import gov.nist.oar.distrib.web.RPAConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.Key;
 import java.security.KeyStore;
 
 public class JKSKeyRetriever implements KeyRetriever {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(JKSKeyRetriever.class);
     @Override
     public Key getKey(RPAConfiguration rpaConfiguration)  {
         try {
+            LOGGER.info("Using Java Keystore located at " + rpaConfiguration.getJksConfig().getKeyStorePath());
             KeyStore keystore = KeyStore.getInstance(rpaConfiguration.getJksConfig().getKeyStoreType());
             keystore.load(Files.newInputStream(
                             Paths.get(rpaConfiguration.getJksConfig().getKeyStorePath())),
@@ -19,7 +25,7 @@ public class JKSKeyRetriever implements KeyRetriever {
                     rpaConfiguration.getJksConfig().getKeyPassword().toCharArray()
             );
         } catch (Exception e) {
-            throw new RuntimeException("Exception during key generation");
+            throw new RuntimeException("Exception during key generation - " + e.getMessage());
         }
     }
 }
