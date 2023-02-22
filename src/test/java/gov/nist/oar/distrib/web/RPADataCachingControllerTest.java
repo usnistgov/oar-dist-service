1d754dd3d29bebb11a69f233a0c669bc57fb7d49
elmiomar <omarilias.elmimouni@nist.gov>
2023-02-22 12:41:12 -0500

    update tests

--- src/test/java/gov/nist/oar/distrib/web/RPADataCachingControllerTest.java
+++ src/test/java/gov/nist/oar/distrib/web/RPADataCachingControllerTest.java
@@ -38,41 +38,37 @@ public class RPADataCachingControllerTest {
 
     @Test
     public void testCacheDataset() {
-//        String dsid = "849E1CC6FBE2C4C7E053B3570681FE987034";
-//        HttpEntity<String> req = new HttpEntity<String>(null, headers);
-//        ResponseEntity<String> resp = websvc.exchange(getBaseURL() + "/ds/restricted/" + dsid,
-//                HttpMethod.PUT, req, String.class);
-//
-//        String randomId = resp.getBody();
-//        assertEquals(HttpStatus.OK, resp.getStatusCode());
-//        assertTrue(randomId.contains("https://data.nist.gov/pdr/datacart/restricted_datacart?id="));
-//        assertEquals(
-//                randomId.length(),
-//                "https://data.nist.gov/pdr/datacart/restricted_datacart?id=".length() + 20
-//        ); // random id len = 20
+        String dsid = "849E1CC6FBE2C4C7E053B3570681FE987034";
+        HttpEntity<String> req = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> resp = websvc.exchange(getBaseURL() + "/ds/restricted/" + dsid,
+                HttpMethod.PUT, req, String.class);
+
+        String randomId = resp.getBody();
+        assertEquals(HttpStatus.OK, resp.getStatusCode());
+        assertEquals(randomId.length(), 20); // random id len = 20
     }
 
     @Test
     public void testRetrieveMetadata() {
-//        String dsid = "849E1CC6FBE2C4C7E053B3570681FE987034";
-//
-//        // first we cache dataset
-//        HttpEntity<String> req = new HttpEntity<String>(null, headers);
-//        ResponseEntity<String> resp = websvc.exchange(getBaseURL() + "/ds/restricted/" + dsid,
-//                HttpMethod.PUT, req, String.class);
-//
-//        assertEquals(HttpStatus.OK, resp.getStatusCode());
-//        //get randomId
-//        String randomId = resp.getBody();
-//
-//        // use randomId to fetch metadata
-//        req = new HttpEntity<String>(null, headers);
-//        ParameterizedTypeReference<Map<String, Object>> responseType =
-//                new ParameterizedTypeReference<Map<String,Object>>() {};
-//        ResponseEntity<Map<String, Object>> response = websvc.exchange(getBaseURL() + "/ds/restricted/" + randomId,
-//                HttpMethod.GET, req, responseType);
-//        assertEquals(HttpStatus.OK, resp.getStatusCode());
-//        Map<String, Object> metadata = response.getBody();
-//        assertEquals(metadata.size(),3 );
+        String dsid = "849E1CC6FBE2C4C7E053B3570681FE987034";
+
+        // first we cache dataset
+        HttpEntity<String> req = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> resp = websvc.exchange(getBaseURL() + "/ds/restricted/" + dsid,
+                HttpMethod.PUT, req, String.class);
+
+        assertEquals(HttpStatus.OK, resp.getStatusCode());
+        //get randomId
+        String randomId = resp.getBody();
+
+        // use randomId to fetch metadata
+        req = new HttpEntity<String>(null, headers);
+        ParameterizedTypeReference<Map<String, Object>> responseType =
+                new ParameterizedTypeReference<Map<String,Object>>() {};
+        ResponseEntity<Map<String, Object>> response = websvc.exchange(getBaseURL() + "/ds/restricted/" + randomId,
+                HttpMethod.GET, req, responseType);
+        assertEquals(HttpStatus.OK, resp.getStatusCode());
+        Map<String, Object> metadata = response.getBody();
+        assertEquals(metadata.size(),3 );
     }
 }
