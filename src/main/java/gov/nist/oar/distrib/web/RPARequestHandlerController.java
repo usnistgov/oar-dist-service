6996c93b82f8a404934753fff6a95b54e41d0019
elmiomar <omarilias.elmimouni@nist.gov>
2023-02-23 00:37:50 -0500

    troubleshoot cors error

--- src/main/java/gov/nist/oar/distrib/web/RPARequestHandlerController.java
+++ src/main/java/gov/nist/oar/distrib/web/RPARequestHandlerController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
         name = "Restricted Public Access Request Handler API",
         description = "These endpoints will handle end user request to download data under restricted public access."
 )
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4201"})
 @RequestMapping(value = "/ds/rpa")
 public class RPARequestHandlerController {
     @Autowired
