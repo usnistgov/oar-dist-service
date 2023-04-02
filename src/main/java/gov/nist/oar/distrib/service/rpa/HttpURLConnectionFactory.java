eb91a8dd6919f99690fce221c736fa0a657ecd80
elmiomar <omarilias.elmimouni@nist.gov>
2023-04-02 08:39:59 -0400

    add a HttpURLConnection facotry as convenience for unit tests

--- src/main/java/gov/nist/oar/distrib/service/rpa/HttpURLConnectionFactory.java
+++ src/main/java/gov/nist/oar/distrib/service/rpa/HttpURLConnectionFactory.java
@@ -5,34 +5,41 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
- * The ConnectionFactory interface provides a method for creating instances
+ * The HttpURLConnectionFactory interface provides a method for creating instances
  * of {@link HttpURLConnection} based on a given {@link URL}.
- * <p>
- * This interface allows for the decoupling of the process of creating
- * HttpURLConnection instances from the classes that use them, making it
+ * <pre></pre>
+ * This interface allows decoupling the creation of HttpURLConnection instances from the classes that use them, making it
  * easier to test and modify the connection creation process if needed.
  * <p>
+ * This design is important when unit testing classes that use HttpURLConnection; we can replace
+ * the HttpURLConnectionFactory implementation with one that returns mocked connections.
+ *
+ * <pre></pre>
  * Example usage:
  * <pre>
  * {@code
- * ConnectionFactory connectionFactory = url -> (HttpURLConnection) url.openConnection();
- *
+ * HttpURLConnectionFactory connectionFactory = new HttpURLConnectionFactory() {
+ *     @Override
+ *     public HttpURLConnection createHttpURLConnection(URL url) throws IOException {
+ *          return (HttpURLConnection) url.openConnection();
+ *     }
+ * };
  * URL requestUrl = new URL("https://example.com");
  * HttpURLConnection connection = connectionFactory.createHttpURLConnection(requestUrl);
  * }
  * </pre>
  * <p>
- * To use a custom ConnectionFactory implementation for testing or other purposes,
- * create a class that implements the ConnectionFactory interface and provide the
+ * To use a custom HttpURLConnectionFactory implementation for testing or other purposes,
+ * create a class that implements the HttpURLConnectionFactory interface and provide the
  * custom implementation in the createHttpURLConnection method.
  */
 public interface HttpURLConnectionFactory {
     /**
-     * Creates a new {@link HttpURLConnection} instance for the specified {@link URL}.
+     * Creates a new {@link HttpURLConnection} instance for the given {@link URL}.
      *
      * @param url the URL for which to create an HttpURLConnection instance
-     * @return a new HttpURLConnection instance for the specified URL
-     * @throws IOException if an I/O exception occurs while opening the connection
+     * @return a new HttpURLConnection instance for the given URL
+     * @throws IOException if an error occurs while opening the connection
      */
     HttpURLConnection createHttpURLConnection(URL url) throws IOException;
 }
