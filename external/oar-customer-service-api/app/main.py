54aa51fa6428792a9479eb9d4b50961722294098
elmiomar <omarilias.elmimouni@nist.gov>
2023-04-29 23:14:55 -0400

    upadte models

--- external/oar-customer-service-api/app/main.py
+++ external/oar-customer-service-api/app/main.py
@@ -3,12 +3,12 @@ import os
 import random
 import uuid
 from datetime import datetime, timedelta
-from typing import Dict, Optional
+from typing import Optional, Tuple
 
 import jwt
-from fastapi import Depends, FastAPI, Header, HTTPException, Query, Request
+from fastapi import Depends, FastAPI, Header, HTTPException, Query
 from fastapi.openapi.utils import get_openapi
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from jwt.exceptions import InvalidTokenError
@@ -17,7 +17,16 @@ from tinydb.table import Table
 
 from app import VERSION
 from app.email_sender import EmailSenderFactory
-from app.models import CreateRecord, EmailInfo, EmailStatus, Record, RecordUpdate
+from app.models import (
+    CreateRecord,
+    EmailInfo,
+    EmailStatus,
+    OauthToken,
+    Record,
+    RecordUpdate,
+    RecordUpdateResponse,
+    RecordWrapper,
+)
 
 # Define path for different endpoints
 PDRCASE_OAUTH2_ENDPOINT = os.environ["PDRCASE_OAUTH2_ENDPOINT"]
@@ -30,15 +39,14 @@ PDRCASE_TEST_ENDPOINT = os.environ["PDRCASE_TEST_ENDPOINT"]
 # Get uvicorn logger
 logger = logging.getLogger("uvicorn")
 
-app = FastAPI()
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+from fastapi.staticfiles import StaticFiles
 
-# Serve Swagger UI files statically
-app.mount("/swagger", StaticFiles(directory="swagger"), name="swagger")
-
-# Serve custom index.html for Swagger UI
-templates = Jinja2Templates(directory="templates")
-
-# Custom OpenAPI documentation
+app = FastAPI(title="OAR Customer Service API", docs_url=None, redoc_url=None)
 
 
 def custom_openapi():
@@ -50,6 +58,9 @@ def custom_openapi():
         description="This is a reference customers service API implementation using FastAPI. It provides several endpoints for creating, retrieving, updating, and deleting records, as well as sending emails. The goal of this app is to serve as a replacement of the Salesforce backend for the RPA Request Handler, in case of any sort of technical issues. The app uses TinyDB as a database.",
         routes=app.routes,
     )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
     # Modify the schema as needed
     app.openapi_schema = openapi_schema
     return app.openapi_schema
@@ -57,10 +68,42 @@ def custom_openapi():
 
 app.openapi = custom_openapi
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/docs", response_class=HTMLResponse, tags=["Doc"])
-async def read_docs(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+        swagger_favicon_url="/static/customer-service.png",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+        redoc_favicon_url="/static/customer-service.png",
+    )
+
+
+favicon_path = "static/favicon.ico"
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
 
 
 def get_db() -> TinyDB:
@@ -68,7 +111,7 @@ def get_db() -> TinyDB:
     Returns an instance of TinyDB for storing records.
 
     Returns:
-        TinyDB: An instance of TinyDB for storing records.
+    - TinyDB: An instance of TinyDB for storing records.
     """
     records_db = TinyDB(
         "app/db.json",
@@ -88,7 +131,7 @@ def get_public_keys_db() -> TinyDB:
     Returns an instance of TinyDB for storing public keys.
 
     Returns:
-        TinyDB: An instance of TinyDB for storing public keys.
+    - TinyDB: An instance of TinyDB for storing public keys.
     """
     # root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     # public_keys_file = os.path.join(root_dir, "keys", "public_keys.json")
@@ -114,10 +157,10 @@ def is_valid_oauth2_token(token: str) -> bool:
     Checks if the given OAuth2 token is valid.
 
     Args:
-        token (str): The OAuth2 token to check.
+    - token (str): The OAuth2 token to check.
 
     Returns:
-        bool: True if the token is valid, False otherwise.
+    - bool: True if the token is valid, False otherwise.
     """
     try:
         # Load the secret key and expected audience from environment variables
@@ -139,10 +182,10 @@ def get_oauth2_token(header: str) -> Optional[str]:
     Extract the OAuth2 token from the "Authorization" header.
 
     Args:
-        header (str): The "Authorization" header value.
+    - header (str): The "Authorization" header value.
 
     Returns:
-        str: The OAuth2 token if it is present in the header, else None.
+    - str: The OAuth2 token if it is present in the header, else None.
     """
     if not header:
         return None
@@ -166,11 +209,11 @@ class TokenStore:
     A class for storing OAuth2 access tokens.
 
     Attributes:
-        tokens (dict): A dictionary that maps usernames to access tokens.
+    - tokens (dict): A dictionary that maps usernames to access tokens.
 
     Methods:
-        get_token(username): Returns the access token for the given username, or None if not found.
-        set_token(username, token, expiration_time): Stores the access token and its expiration time for the given username.
+    - get_token(username): Returns the access token for the given username, or None if not found.
+    - set_token(username, token, expiration_time): Stores the access token and its expiration time for the given username.
     """
 
     def __init__(self):
@@ -181,10 +224,10 @@ class TokenStore:
         Returns the access token for the given username, or None if not found.
 
         Args:
-            username (str): The username for which to get the access token.
+        - username (str): The username for which to get the access token.
 
         Returns:
-            str or None: The access token for the given username, or None if not found.
+        - str or None: The access token for the given username, or None if not found.
         """
         existing_token = self.tokens.get(username)
         return existing_token if existing_token else None
@@ -194,9 +237,9 @@ class TokenStore:
         Stores the access token and its expiration time for the given username.
 
         Args:
-            username (str): The username for which to store the access token.
-            token (str): The access token to store.
-            expiration_time (datetime): The expiration time of the access token.
+        - username (str): The username for which to store the access token.
+        - token (str): The access token to store.
+        - expiration_time (datetime): The expiration time of the access token.
         """
         self.tokens[username] = {
             "access_token": token,
@@ -213,14 +256,14 @@ def get_decoded_payload(assertion: str, db: TinyDB) -> dict:
     Decodes the given JWT assertion and verifies its signature using the public key stored in the database.
 
     Args:
-        assertion (str): The JWT assertion to decode.
-        db (TinyDB): The TinyDB instance used to access the public keys database.
+    - assertion (str): The JWT assertion to decode.
+    - db (TinyDB): The TinyDB instance used to access the public keys database.
 
     Returns:
-        dict: The decoded JWT payload as a dictionary.
+    - dict: The decoded JWT payload as a dictionary.
 
     Raises:
-        HTTPException: If the client_id in the JWT assertion is not found in the public keys database.
+    - HTTPException: If the client_id in the JWT assertion is not found in the public keys database.
 
     """
     # Decode the JWT assertion without verifying the signature
@@ -254,16 +297,16 @@ def get_decoded_payload(assertion: str, db: TinyDB) -> dict:
     return payload
 
 
-def generate_access_token(payload: dict) -> dict:
+def generate_access_token(payload: dict) -> Tuple[str, str]:
     """
     Generates an access token for the given JWT payload.
     Checks if a token already exists token and not expired.
 
     Args:
-        payload (dict): The JWT payload as a dictionary.
+    - payload (dict): The JWT payload as a dictionary.
 
     Returns:
-        dict: A dictionary containing the access token and the instance URL.
+    - dict: A dictionary containing the access token and the instance URL.
     """
     # Extract client_id, username, audience, and expiration time
     client_id = payload["iss"]
@@ -281,10 +324,7 @@ def generate_access_token(payload: dict) -> dict:
         >= datetime.utcnow()
     ):
         instance_url = os.environ["INSTANCE_URL"]
-        return {
-            "access_token": existing_token["access_token"],
-            "instance_url": instance_url,
-        }
+        return (existing_token["access_token"], instance_url)
     else:
         # If stored token is not found or has expired, generate a new token
         exp_time = int(
@@ -309,11 +349,16 @@ def generate_access_token(payload: dict) -> dict:
         # Store token and expiration time
         token_store.set_token(username, token, exp_time)
 
-        return {"access_token": token, "instance_url": instance_url}
+        return (token, instance_url)
 
 
 # Get access token endpoint
-@app.post(f"{PDRCASE_OAUTH2_ENDPOINT}", tags=["Auth"])
+@app.post(
+    f"{PDRCASE_OAUTH2_ENDPOINT}",
+    response_model=OauthToken,
+    status_code=200,
+    tags=["Auth"],
+)
 def get_token(
     grant_type: str = Query(..., description="The grant type."),
     assertion: str = Query(..., description="The JWT assertion."),
@@ -328,7 +373,8 @@ def get_token(
             raise HTTPException(status_code=400, detail={"error": str(e)})
 
         # Generate token
-        return generate_access_token(payload)
+        access_token, instance_url = generate_access_token(payload)
+        return OauthToken(access_token=access_token, instance_url=instance_url)
 
     else:
         raise HTTPException(status_code=400, detail={"error": "unsupported_grant_type"})
@@ -337,7 +383,7 @@ def get_token(
 # Get record endpoint
 @app.get(
     f"{PDRCASE_GET_ENDPOINT}/{{id}}",
-    response_model=Dict[str, Record],
+    response_model=RecordWrapper,
     status_code=200,
     tags=["Record"],
 )
@@ -348,16 +394,18 @@ async def get_record(
     Retrieve a record by its ID.
 
     Args:
-        id (str): The ID of the record to retrieve.
-        token (str): A bearer token for authentication.
+    - id (str): The ID of the record to retrieve.
+    - Authorization (str, optional): A bearer token for authentication. Defaults to None.
+    - db (TinyDB, optional): A dependency that provides a database connection. Defaults to Depends(get_db).
 
     Returns:
-        dict: A dictionary containing a single key-value pair, where the key is "record"
-            and the value is a Record object.
+    - dict: A dictionary containing a single key-value pair, where the key is "record" and the value is a Record object.
 
     Raises:
-        HTTPException: If the token is missing or invalid, or if the record is not found.
+    - HTTPException: If the bearer token is missing or invalid, or if the record is not found.
+    ```
     """
+
     logger.info(f"Getting record with ID = {id}")
     token = get_oauth2_token(Authorization)
     # Check if token is missing in the header
@@ -371,20 +419,23 @@ async def get_record(
     records_table = db.table("records")
 
     # Retrieve the record from the "records" table
-    record = records_table.get(doc_id=id)
+    record_data = records_table.get(doc_id=id)
 
-    if record is None:
+    if record_data is None:
         # If the record is not found, raise an HTTPException with status code 404
         raise HTTPException(status_code=404, detail="Record not found")
 
-    # Create a Record object and return it
-    return {"record": record}
+    # Instantiate the Record object
+    record = Record(**record_data)
+
+    # Instantiate the RecordWrapper object
+    return RecordWrapper(record=record)
 
 
 # Create Record endpoint
 @app.post(
     PDRCASE_CREATE_ENDPOINT,
-    response_model=Dict[str, Record],
+    response_model=RecordWrapper,
     status_code=200,
     tags=["Record"],
 )
@@ -397,16 +448,14 @@ async def create_record(
     Create a new record in the database.
 
     Args:
-        data (CreateRecord): A CreateRecord object containing the data for the new record.
-        token (str): A bearer token for authentication.
+    - data (CreateRecord): A CreateRecord object containing the data for the new record.
+    - token (str): A bearer token for authentication.
 
     Returns:
-        dict: A dictionary containing a single key-value pair, where the key is "record"
-            and the value is a Record object.
+    - dict: A dictionary containing a single key-value pair, where the key is "record" and the value is a Record object.
 
     Raises:
-        HTTPException: If the token is missing or invalid, or if there is an error
-            inserting the record into the database.
+    - HTTPException: If the token is missing or invalid, or if there is an error inserting the record into the database.
     """
     logger.info(f"Creating a new record")
     # Check if token is missing in the header
@@ -430,14 +479,15 @@ async def create_record(
     except Exception as e:
         # If there is an error inserting the record, raise an HTTPException with status code 500
         raise HTTPException(status_code=500, detail="Error creating record" + str(e))
-    # Return the new record in a dictionary format with a single key-value pair
-    return {"record": record}
+
+    # Create a RecordWrapper object and return it
+    return RecordWrapper(record=record)
 
 
 # Update Record endpoint
 @app.patch(
     f"{PDRCASE_UPDATE_ENDPOINT}/{{id}}",
-    response_model=Dict[str, str],
+    response_model=RecordUpdateResponse,
     status_code=200,
     tags=["Record"],
 )
@@ -451,17 +501,15 @@ async def update_record(
     Update the approval status of a record.
 
     Args:
-        id (str): The ID of the record to update.
-        update (RecordUpdate): A RecordUpdate object containing the new approval status.
-        token (str): A bearer token for authentication.
+    - id (str): The ID of the record to update.
+    - update (RecordUpdate): A RecordUpdate object containing the new approval status.
+    - token (str): A bearer token for authentication.
 
     Returns:
-        dict: A dictionary containing two key-value pair, one pair for recordId,
-        the second for approvalStatus.
+    - dict: A dictionary containing two key-value pair, one pair for recordId, the second for approvalStatus.
 
     Raises:
-        HTTPException: If the token is missing or invalid, if the record is not found,
-            or if there is an error updating the record in the database.
+    - HTTPException: If the token is missing or invalid, if the record is not found, or if there is an error updating the record in the database.
     """
     logger.info(f"Updating record with ID = {id}")
     # Check if token is missing in the header
@@ -515,14 +563,14 @@ async def send_email(
     Send an email with the provided email information.
 
     Args:
-        email_info (EmailInfo): An EmailInfo object containing the email information.
-        token (str): A bearer token for authentication.
+    - email_info (EmailInfo): An EmailInfo object containing the email information.
+    - token (str): A bearer token for authentication.
 
     Returns:
-        EmailStatus: An EmailStatus object containing the email information and a timestamp.
+    - EmailStatus: An EmailStatus object containing the email information and a timestamp.
 
     Raises:
-        HTTPException: If the token is missing or invalid.
+    - HTTPException: If the token is missing or invalid.
     """
     logger.info(
         f"Sending email to '{email_info.recipient}' with subject '{email_info.subject}'"
--- external/oar-customer-service-api/app/models.py
+++ external/oar-customer-service-api/app/models.py
@@ -1,5 +1,18 @@
-from typing import Dict
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+
+class OauthToken(BaseModel):
+    access_token: str
+    instance_url: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "access_token": "00D1g0000000abc!AQcAQLOpWfY8jJ4G4aP7....",
+                "instance_url": "https://test.example.com",
+            }
+        }
 
 
 class UserInfo(BaseModel):
@@ -13,15 +26,71 @@ class UserInfo(BaseModel):
     subject: str
     description: str
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullName": "Omar Ilias EL MIMOUNI",
+                "organization": "NIST",
+                "email": "omarilias.elmimouni@nist.gov",
+                "receiveEmails": "True",
+                "country": "United States",
+                "approvalStatus": "Pending",
+                "productTitle": "Collaborative Guarded-Hot-Plate Tests between the National Institute of Standards and Technology and the National Physical Laboratory",
+                "subject": "849E1CC6FBE2C4C7E053B3570681FE987034",
+                "description": "Product Title:\n  Data from: Collaborative Guarded-Hot-Plate Tests between the National Institute of Standards and Technology and the National Physical Laboratory \n\n Purpose of Use: \nLearning and Research",
+            }
+        }
+
 
 class Record(BaseModel):
     id: str
     caseNum: str
     userInfo: UserInfo
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "5dc03d34-3cef-4238-9dcf-678302694c46",
+                "caseNum": "5678",
+                "userInfo": {
+                    "fullName": "Omar Ilias EL MIMOUNI",
+                    "organization": "NIST",
+                    "email": "omarilias.elmimouni@nist.gov",
+                    "receiveEmails": "True",
+                    "country": "United States",
+                    "approvalStatus": "Pending",
+                    "productTitle": "Collaborative Guarded-Hot-Plate Tests between the National Institute of Standards and Technology and the National Physical Laboratory",
+                    "subject": "849E1CC6FBE2C4C7E053B3570681FE987034",
+                    "description": "Product Title:\n  Data from: Collaborative Guarded-Hot-Plate Tests between the National Institute of Standards and Technology and the National Physical Laboratory \n\n Purpose of Use: \nLearning and Research",
+                },
+            }
+        }
+
+
+class RecordWrapper(BaseModel):
+    record: Record
+
 
 class RecordUpdate(BaseModel):
     Approval_Status__c: str
+
+    class Config:
+        schema_extra = {
+            "example": {"Approval_Status__c": "Approved_2023-04-25T10:00:00.000Z"}
+        }
+
+
+class RecordUpdateResponse(BaseModel):
+    recordId: str
+    approvalStatus: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "recordId": "5dc03d34-3cef-4238-9dcf-678302694c46",
+                "approvalStatus": "Approved_2023-04-25T10:00:00.000Z",
+            }
+        }
 
 
 class CreateRecord(BaseModel):
@@ -35,16 +104,12 @@ class EmailInfo(BaseModel):
     content: str
 
     class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
         schema_extra = {
             "example": {
                 "recordId": "bca9a7508cda11eda5b42a58cc9fff89",
                 "recipient": "elmimouni.o.i@gmail.com",
                 "subject": "Test email",
-                "content": """<h1>Test Email</h1>
-                              <div>Congratulations, it is working.</div>
-                           """,
+                "content": "<h1>Test Email</h1><div>Congratulations, it is working.</div>",
             }
         }
 
@@ -52,3 +117,16 @@ class EmailInfo(BaseModel):
 class EmailStatus(BaseModel):
     email_info: EmailInfo
     timestamp: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                "email_info": {
+                    "recordId": "bca9a7508cda11eda5b42a58cc9fff89",
+                    "recipient": "elmimouni.o.i@gmail.com",
+                    "subject": "Test email",
+                    "content": "<h1>Test Email</h1><div>Congratulations, it is working.</div>",
+                },
+            }
+        }
