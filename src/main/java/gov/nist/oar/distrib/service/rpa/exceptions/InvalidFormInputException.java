9a6d17f693a29e6f3392e9bbf38cdb2b23109f7a
elmiomar <elmimouni.o.i@gmail.com>
2023-06-01 10:57:17 -0400

    add exception for invalid form input

--- src/main/java/gov/nist/oar/distrib/service/rpa/exceptions/InvalidFormInputException.java
+++ src/main/java/gov/nist/oar/distrib/service/rpa/exceptions/InvalidFormInputException.java
@@ -1,4 +1,17 @@
 package gov.nist.oar.distrib.service.rpa.exceptions;
 
-public class FormInputException {
+/**
+ * Exception thrown when a form input is invalid.
+ */
+public class InvalidFormInputException extends RuntimeException {
+
+    /**
+     * Constructs a new InvalidFormInputException with the specified detail message.
+     *
+     * @param message the detail message of the exception
+     */
+    public InvalidFormInputException(String message) {
+        super(message);
+    }
 }
+
